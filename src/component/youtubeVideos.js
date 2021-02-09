57735c9c14f1f92106f18a32d7885576353fe791
Koki Satani <n20010@std.it-college.ac.jp>
2021-02-09 07:22:39 +0000

    Youtubeから取得した動画データの表示に成功

--- src/component/youtubeVideos.js
+++ src/component/youtubeVideos.js
@@ -1,1 +1,24 @@
-import 
+import React from 'react'
+
+const YoutubeVideos = props => {
+  const video = props.videos.map(video => {
+    const url = 'https://www.youtube.com/embed/' + video.id.videoId
+
+    return (
+      <div key={video.id.videoId}>
+        <iframe
+          id='ytplayer'
+          type='ytplayer'
+          width='480'
+          height='270'
+          src={url}
+          frameborder='0'
+        />
+      </div>
+    )
+  })
+
+  return <div>{video}</div>
+}
+
+export default YoutubeVideos
